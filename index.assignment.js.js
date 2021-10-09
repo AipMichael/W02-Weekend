064a8905ea98df60480b2e1f54ab096fbc96f39a
AipBorges <mpbds@uic.es>
2021-10-09 13:55:57 +0200

    debugging: the function is not working. did the first debugging part.

--- index.assignment.js.js
+++ index.assignment.js.js
@@ -1,5 +1,3 @@
-// crear array de 5x5
-
 const board = [
   [0, 0, 0, 0, 0],
   [0, 0, 1, 0, 0],
@@ -10,23 +8,12 @@ const board = [
 
 console.table(board);
 
-const isOne = (item) => {
-  item === 1;
-};
-const isZero = (item) => {
-  item === 0;
-};
 let newBoard;
 
-let i = 0;
-let j = 0;
-
-
-
-const liveNeighbors = (theBoard, i, j) => {
-  const neighbors = 0;
-  const width = board[i].length-1;
-  const height = board.length-1; 
+const livingNeighbors = (theBoard, i, j) => {
+  let neighbors = 0;
+  const width = board[i].length - 1;
+  const height = board.length - 1;
 
   if (i === 0 && j === 0) {
     // case 1 (corner left top)
@@ -38,62 +25,53 @@ const liveNeighbors = (theBoard, i, j) => {
     if (board[i][j - 1] === 1) neighbors++;
     if (board[i + 1][j - 1] === 1) neighbors++;
     if (board[i + 1][j] === 1) neighbors++;
-  } else if (i === height && j === width){ 
-    //case 3 (corner right down)
-    if (board[i - 1][j - 1]) neighbors++, 
-    if (board[i - 1][j]) neighbors++, 
-    if (board[i][j - 1]) neighbors++,
-  } else if (i === height, j === 0){ 
+  } else if (i === height && j === width) {
+    // case 3 (corner right down)
+    if (board[i - 1][j - 1] === 1) neighbors++;
+    if (board[i - 1][j] === 1) neighbors++;
+    if (board[i][j - 1] === 1) neighbors++;
+  } else if ((i === height, j === 0)) {
     // case 4 (corner left bottom)
-    if (board[i][j + 1]) neighbors++, 
-    if (board[i - 1][j + 1]) neighbors++, 
-    if (board[i - 1][j]) neighbors++,
-
-  } else if (i > 0 && i < height && j === 0){
-
-    // case 5 (left line) 
-    if (board[i - 1][j]) neighbors++,
-    if (board[i - 1][j + 1]) neighbors++,
-    if (board[i][j + 1]) neighbors++,
-    if (board[i + 1][j]) neighbors++,
-    if (board[i + 1][j + 1]) neighbors++,
-
-  } else if (i === 0 && (j > 0 && j < width)){ 
-
+    if (board[i][j + 1] === 1) neighbors++;
+    if (board[i - 1][j + 1] === 1) neighbors++;
+    if (board[i - 1][j] === 1) neighbors++;
+  } else if (i > 0 && i < height && j === 0) {
+    // case 5 (left line)
+    if (board[i - 1][j] === 1) neighbors++;
+    if (board[i - 1][j + 1] === 1) neighbors++;
+    if (board[i][j + 1] === 1) neighbors++;
+    if (board[i + 1][j] === 1) neighbors++;
+    if (board[i + 1][j + 1] === 1) neighbors++; // option 1
+  } else if (i === 0 && j > 0 && j < width) {
     // case 6 (top line)
-    if (board[i][j - 1]) neighbors++,
-    if (board[i][j + 1]) neighbors++,
-    if (board[i + 1][j - 1]) neighbors++,
-    if (board[i + 1][j]) neighbors++,
-    if (board[i + 1][j + 1]) neighbors++
-
-  } else if (i > 0 && i < height && j === width){ 
-
+    if (board[i][j - 1] === 1) neighbors++;
+    if (board[i][j + 1] === 1) neighbors++;
+    if (board[i + 1][j - 1] === 1) neighbors++;
+    if (board[i + 1][j] === 1) neighbors++;
+    if (board[i + 1][j + 1] === 1) neighbors++; // option 2
+  } else if (i > 0 && i < height && j === width) {
     // case 7 (right line)
-    if (board[i - 1][j]) neighbors++,
-    if (board[i - 1][j-1]) neighbors++,
-    if (board[i][j - 1]) neighbors++,
-    if (board[i + 1][j - 1]) neighbors++,
-    if (board[i + 1][j]) neighbors++,
-
-  } else if (i === height && (j > 0 && j < width)){ 
-
+    if (board[i - 1][j] === 1) neighbors++;
+    if (board[i - 1][j - 1] === 1) neighbors++;
+    if (board[i][j - 1] === 1) neighbors++;
+    if (board[i + 1][j - 1] === 1) neighbors++;
+    if (board[i + 1][j] === 1) neighbors++;
+  } else if (i === height && j > 0 && j < width) {
     // case 8 (bottom line)
-    if (board[i][j - 1]) neighbors++,
-    if (board[i - 1][j - 1]) neighbors++,
-    if (board[i - 1][j]) neighbors++,
-    if (board[i - 1][j + 1]) neighbors++,
-    if (board[i + 1][j + 1]) neighbors++,
+    if (board[i][j - 1] === 1) neighbors++;
+    if (board[i - 1][j - 1] === 1) neighbors++;
+    if (board[i - 1][j] === 1) neighbors++;
+    if (board[i - 1][j + 1] === 1) neighbors++;
+    if (board[i][j + 1] === 1) neighbors++;
   } else {
-    if (board[i - 1][j - 1]) neighbors++,
-    if (board[i - 1][j]) neighbors++,
-    if (board[i - 1][j + 1]) neighbors++,
-    if (board[i][j - 1]) neighbors++,
-    if (board[i][j + 1]) neighbors++,
-    if (board[i + 1][j - 1]) neighbors++,
-    if (board[i + 1][j]) neighbors++,
-    if (board[i + 1][j + 1]) neighbors++,
-
+    if (board[i - 1][j - 1] === 1) neighbors++;
+    if (board[i - 1][j] === 1) neighbors++;
+    if (board[i - 1][j + 1] === 1) neighbors++;
+    if (board[i][j - 1] === 1) neighbors++;
+    if (board[i][j + 1] === 1) neighbors++;
+    if (board[i + 1][j - 1] === 1) neighbors++;
+    if (board[i + 1][j] === 1) neighbors++;
+    if (board[i + 1][j + 1] === 1) neighbors++;
   }
   return neighbors;
 };
@@ -101,25 +79,24 @@ const liveNeighbors = (theBoard, i, j) => {
 const boardLoop = (currentBoard) => {
   newBoard = currentBoard.slice();
 
-  for (i; i < newBoard.length; i++) {
-    for (j; j < newBoard[i].length; j++) {
-      const neighbors = liveNeighbors(currentBoard, i, j);
+  for (let i = 0; i < newBoard.length; i++) {
+    for (let j = 0; j < newBoard[i].length; j++) {
+      const numberNeighbors = livingNeighbors(currentBoard, i, j);
 
-      if (isOne(currentBoard[i][j]) && neighbors >= 2 && neighbors < 4) {
-        newBoard[i][j] = 1;
-      }
-      if (isOne(currentBoard[i][j]) && neighbors < 2 && neighbors > 4) {
-        newBoard[i][j] = 0;
-        return;
-      }
-      if (isZero(currentBoard[i][j], i, j) && neighbors === 3) {
-        newBoard[i][j] = 1;
+      if (currentBoard[i][j] === 1) {
+        if (numberNeighbors < 2 || numberNeighbors > 4) {
+          newBoard[i][j] = 0;
+        } else if (numberNeighbors > 1 && numberNeighbors < 4) {
+          newBoard[i][j] = 1;
+        }
+      } else if (currentBoard[i][j] === 0) {
+        if (numberNeighbors === 3) {
+          newBoard[i][j] = 1;
+        }
       }
     }
   }
-  return newBoard;
+  console.table(newBoard);
 };
 
-newBoard = boardLoop(board);
-
-console.log(newBoard);
+boardLoop(board);
